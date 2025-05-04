9fc6ad0ca66a0033764d1740f2e04730967c8f25
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-05-04 19:34:56 -0300

    Lista 4 - Exercicío 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int numero;
+
+Console.Write("Digite um número: ");
+numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0){
+    Console.WriteLine("{0} é par", numero);
+}
+
+else {
+    Console.WriteLine("{0} é ímpar", numero);
+}
